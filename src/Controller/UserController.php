de40e09f3bb5aa55f7006fe7a7718a6612622bd6
StyKon <76910363+StyKon@users.noreply.github.com>
2021-01-20 07:41:21 +0100

    StyKon Symfony

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,12 +9,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface; 
 
 /**
  * @Route("/user")
  */
 class UserController extends AbstractController
 {
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+    $this->passwordEncoder = $passwordEncoder;
+    }
     /**
      * @Route("/", name="user_index", methods={"GET"})
      */
@@ -36,6 +42,11 @@ class UserController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
+            // @todo: refactor
+$plainpwd = $user->getPassword();
+$encoded = $this->passwordEncoder->encodePassword($user, $plainpwd);
+$user->setPassword($encoded);         
+ 
             $entityManager->persist($user);
             $entityManager->flush();
 
@@ -67,7 +78,13 @@ class UserController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            // @todo: refactor
+$entityManager = $this->getDoctrine()->getManager();
+$plainpwd = $user->getPassword();
+$encoded = $this->passwordEncoder->encodePassword($user, $plainpwd);
+$user->setPassword($encoded);
+$entityManager->persist($user);
+$entityManager->flush();
 
             return $this->redirectToRoute('user_index');
         }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,17 +6,38 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\CallbackTransformer;
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('username')
-            ->add('roles')
+            ->add('Roles', ChoiceType::class, [
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'choices'  => [
+                  'User' => 'ROLE_USER',
+                  'Partner' => 'ROLE_PARTNER',
+                  'Admin' => 'ROLE_ADMIN',
+                ],
+            ])
             ->add('password')
             ->add('Phone')
         ;
+        $builder->get('Roles')
+    ->addModelTransformer(new CallbackTransformer(
+        function ($rolesArray) {
+             // transform the array to a string
+             return count($rolesArray)? $rolesArray[0]: null;
+        },
+        function ($rolesString) {
+             // transform the string back to an array
+             return [$rolesString];
+        }
+        ));
     }
 
     public function configureOptions(OptionsResolver $resolver)
